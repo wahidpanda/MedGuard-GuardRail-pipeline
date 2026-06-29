6e518a66bd31486d7db1c9a854b74b54b8f799c2
unknown <islamoahidul12@gmail.com>
2026-06-29 19:29:24 +0600

    added app.jsx

---
+++ frontend/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: { port: 5173, proxy: { '/api': 'http://localhost:8000' } },
+  preview: { port: 5173, proxy: { '/api': 'http://localhost:8000' } }
+})
